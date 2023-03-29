605b5e3f060573a03135a3d09a468a1ba824b642
Akshay-Jayaram <109137330+Akshay-Jayaram@users.noreply.github.com>
2023-03-29 16:26:57 +0530

    Initial files

---
+++ index.js
@@ -0,0 +1,26 @@
+//loading dice image for player 1
+var randomNumber1= Math.floor(Math.random()*6)+1;
+
+var image1= "images/dice"+ randomNumber1 +".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", image1);
+
+
+//loading dice image for player 1
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var image2= "images/dice"+ randomNumber2 +".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",image2);
+
+
+//Printing result
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
